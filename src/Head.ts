02dd96f614926785773cd0f1dce2f1cafff829e5
Martin <marto.sobel@gmail.com>
2022-06-13 10:08:07 -0300

    restricted movements

--- src/Head.ts
+++ src/Head.ts
@@ -3,6 +3,8 @@ import { Node } from "./Node";
 import { Snake } from "./Snake";
 
 export class Head extends Node{
+    public direction = ''
+
     public speed : number = 1
     public name? : string = 'head'
     constructor(
@@ -13,71 +15,83 @@ export class Head extends Node{
     }
 
     public forward(){
-        this.removeComponent(utils.FollowPathComponent)
-        let newPath = []
-        newPath[0] = new Vector3(
-            this.getComponent(Transform).position.x,
-            this.getComponent(Transform).position.y,
-            this.getComponent(Transform).position.z
-        )
-        newPath[1] = new Vector3(
-            this.getComponent(Transform).position.x,
-            this.getComponent(Transform).position.y,
-            64
-        )
-        this.addComponent(new utils.FollowPathComponent(newPath, 4))
-        this.getComponent(Transform).rotation.set(0, 1, 0, -1)
+        if(this.direction != 'NORTH' && this.direction != 'SOUTH'){
+            this.direction = 'NORTH'
+            this.removeComponent(utils.FollowPathComponent)
+            let newPath = []
+            newPath[0] = new Vector3(
+                this.getComponent(Transform).position.x,
+                this.getComponent(Transform).position.y,
+                this.getComponent(Transform).position.z
+            )
+            newPath[1] = new Vector3(
+                this.getComponent(Transform).position.x,
+                this.getComponent(Transform).position.y,
+                64
+            )
+            this.addComponent(new utils.FollowPathComponent(newPath, 4))
+            this.getComponent(Transform).rotation.set(0, 1, 0, -1)
+        }
     }
 
     public turnRigth(){
-        this.removeComponent(utils.FollowPathComponent)
-        let newPath = []
-        newPath[0] = new Vector3(
-            this.getComponent(Transform).position.x,
-            this.getComponent(Transform).position.y,
-            this.getComponent(Transform).position.z
-        )
-        newPath[1] = new Vector3(
-            64,
-            this.getComponent(Transform).position.y,
-            this.getComponent(Transform).position.z
-        )
-        this.addComponent(new utils.FollowPathComponent(newPath, 4))
-        this.getComponent(Transform).rotation.set(0, 0, 0, 1)
+        if(this.direction != 'EAST' && this.direction != 'WEST'){
+            this.direction = 'WEST'
+            this.removeComponent(utils.FollowPathComponent)
+            let newPath = []
+            newPath[0] = new Vector3(
+                this.getComponent(Transform).position.x,
+                this.getComponent(Transform).position.y,
+                this.getComponent(Transform).position.z
+            )
+            newPath[1] = new Vector3(
+                64,
+                this.getComponent(Transform).position.y,
+                this.getComponent(Transform).position.z
+            )
+            this.addComponent(new utils.FollowPathComponent(newPath, 4))
+            this.getComponent(Transform).rotation.set(0, 0, 0, 1)
+        }
     }
 
     public turnLeft(){
-        this.removeComponent(utils.FollowPathComponent)
-        let newPath = []
-        newPath[0] = new Vector3(
-            this.getComponent(Transform).position.x,
-            this.getComponent(Transform).position.y,
-            this.getComponent(Transform).position.z
-        )
-        newPath[1] = new Vector3(
-            0,
-            this.getComponent(Transform).position.y,
-            this.getComponent(Transform).position.z
-        )
-        this.addComponent(new utils.FollowPathComponent(newPath, 4))
-        this.getComponent(Transform).rotation.set(0, 1, 0, 0)
+        if(this.direction != 'EAST' && this.direction != 'WEST'){
+            this.direction = 'EAST'
+            this.removeComponent(utils.FollowPathComponent)
+            let newPath = []
+            newPath[0] = new Vector3(
+                this.getComponent(Transform).position.x,
+                this.getComponent(Transform).position.y,
+                this.getComponent(Transform).position.z
+            )
+            newPath[1] = new Vector3(
+                0,
+                this.getComponent(Transform).position.y,
+                this.getComponent(Transform).position.z
+            )
+            this.addComponent(new utils.FollowPathComponent(newPath, 4))
+            this.getComponent(Transform).rotation.set(0, 1, 0, 0)
+        }
     }
 
     public backward(){
-        this.removeComponent(utils.FollowPathComponent)
-        let newPath = []
-        newPath[0] = new Vector3(
-            this.getComponent(Transform).position.x,
-            this.getComponent(Transform).position.y,
-            this.getComponent(Transform).position.z
-        )
-        newPath[1] = new Vector3(
-            this.getComponent(Transform).position.x,
-            this.getComponent(Transform).position.y,
-            0
-        )
-        this.addComponent(new utils.FollowPathComponent(newPath, 4))
-        this.getComponent(Transform).rotation.set(0, 1, 0, 1)
+        if(this.direction != 'NORTH' && this.direction != 'SOUTH'){
+            this.direction = 'SOUTH'
+            this.removeComponent(utils.FollowPathComponent)
+            let newPath = []
+            newPath[0] = new Vector3(
+                this.getComponent(Transform).position.x,
+                this.getComponent(Transform).position.y,
+                this.getComponent(Transform).position.z
+            )
+            newPath[1] = new Vector3(
+                this.getComponent(Transform).position.x,
+                this.getComponent(Transform).position.y,
+                0
+            )
+            this.addComponent(new utils.FollowPathComponent(newPath, 4))
+            this.getComponent(Transform).rotation.set(0, 1, 0, 1)
+        }
     }
 
     /* Head hits Wall or own Body */ 

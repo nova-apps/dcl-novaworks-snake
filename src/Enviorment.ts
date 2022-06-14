471bfe6e527c6451f6cee4a6423ea1355b7d0009
Martin <marto.sobel@gmail.com>
2022-06-14 12:32:18 -0300

    triggerbox walls

--- src/Enviorment.ts
+++ src/Enviorment.ts
@@ -7,41 +7,7 @@ export class Enviorment {
     constructor(public snake: Snake, public ui: UI){
         this.snake = snake
         this.ui = ui
-        // this.addFloor()
-        // this.addRoof()
-        // this.AddWalls()
-    }
-
-    public addFloor(){
-        const floor = new Entity()
-        floor.addComponent(new PlaneShape())
-        floor.addComponent(new Transform({
-            position: new Vector3(32, 0, 32),
-            rotation: new Quaternion(-1, -1, -1, 1),
-            scale: new Vector3(64, 64, 64)
-        }))
-        
-        let floorMaterial = new Material()
-        floorMaterial.albedoColor = new Color4(0, 0, 0, 0)
-        floor.addComponent(floorMaterial)
-        
-        engine.addEntity(floor)
-    }
-
-    public addRoof(){
-        const roof = new Entity()
-        roof.addComponent(new PlaneShape())
-        roof.addComponent(new Transform({
-            position: new Vector3(32,7, 32),
-            rotation: new Quaternion(-1, -1, -1, 1),
-            scale: new Vector3(64, 64, 64)
-        }))
-        
-        let roofMaterial = new Material()
-        roofMaterial.albedoColor = new Color4(0, 0, 0, 0)
-        roof.addComponent(roofMaterial)
-        
-        engine.addEntity(roof)
+        this.AddWalls()
     }
 
     public createWall(x: number, y: number, z: number, rotation: number){
@@ -55,16 +21,16 @@ export class Enviorment {
         wall.addComponent(new Transform({
             position: new Vector3(x, y, z),
             rotation: new Quaternion(0, 1, 0, rotation),
-            scale: new Vector3(64, 15, 64)
+            scale: new Vector3(32, 4, 32)
         }))
         wall.addComponent(wallMaterial)
         
         let wallTrigger = new utils.TriggerBoxShape()
         
         if(rotation == 0){
-            wallTrigger.size = new Vector3(64, 15, 1)
+            wallTrigger.size = new Vector3(32, 4, 0.3)
         } else {
-            wallTrigger.size = new Vector3(1, 15, 64)
+            wallTrigger.size = new Vector3(0.3, 4, 32)
         }
         
         wall.addComponent(
@@ -74,7 +40,8 @@ export class Enviorment {
                     layer: 1,
                     onTriggerEnter : () => {
                         this.ui.resetScore()
-                    }
+                    },
+                    enableDebug: false
                 },
                 
             )
@@ -84,9 +51,9 @@ export class Enviorment {
     }
 
     public AddWalls(){
-        this.createWall(32, 0, 63, 0)
-        this.createWall(32, 0, 1, 0)
-        this.createWall(1, 0, 32, 1)
-        this.createWall(63, 0, 32, 1)
+        this.createWall(30.5, 0, 16, 1)
+        this.createWall(16, 0, 0.5, 0)
+        this.createWall(1, 0, 16, 1)
+        this.createWall(16, 0, 31, 0)
     }
 }
